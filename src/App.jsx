9b0a3c9bacaf0535d822225dedb4c473eec98fe5
Santhosh Chinnasamy <csesanthosh15@gmail.com>
2024-04-24 14:27:06 +0530

    add protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,41 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { Album } from "./album";
 import Expense from "./expense";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 import Login from "./login";
+import { useCookies } from "react-cookie";
+
+const ProtectedRoute = ({ children }) => {
+  const [cookies] = useCookies();
+  return cookies.token ? children : <Navigate to="/login" />;
+};
 
 const routes = createBrowserRouter([
   {
     path: "/expense",
-    Component: Expense,
+    // Component: Expense,
+    element: (
+      <ProtectedRoute>
+        <Expense />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/",
-    Component: Album,
+    element: (
+      <ProtectedRoute>
+        <Album />
+      </ProtectedRoute>
+    ),
   },
   {
-    path:"/login",
-    Component: Login
-  }
+    path: "/login",
+    Component: Login,
+  },
 ]);
 
 function App() {
